66b280d6a4c65e16c4288c7f2832fe024eaf6aea
Jagadeesh Madagundi <jagadeesh545@gmail.com>
2019-09-07 22:42:08 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Callback from './components/callback';
+// import Navbar from './Navbar';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    url: '/'
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Route path='/callback' component={Callback} />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080')
+      .then((response) => response.text())
+      .then((data) => {
+        // this.setState({url: data});
+        // window.location.assign(this.state.url);
+        window.location.assign(data);
+      })
+      .catch(console.log)
+  }
 }
 
 export default App;
